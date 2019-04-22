229b6bc0d8cdcb23ffae77caffce6f3c263fa059
Puneet Kashyap <pkashyaps@gmail.com>
2019-04-22 15:31:44 -0400

    Initial draft of home page added

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,6 +1,6 @@
 const assets = {
   companyName: 'Mapledge Staffing',
-  contacts : [
+  contacts: [
     {
       name: 'Neeraj Tara',
       phone: '+1-306-999-5295',
@@ -11,7 +11,24 @@ const assets = {
       phone: '+1-306-999-5295',
       email: 'taravarun@gmail.com'
     }
+  ],
+  marketing: [
+    {
+      name: `FIND WORK`,
+      desc: `If you are an Industrial Skilled Trade or General Worker - looking
+            to advance your career or you are a Job Seeker - looking for
+            flexible work schedules or you simply want to make extra money in
+            addition to your main income`,
+      slogan: `YOU CAME TO THE RIGHT PLACE.`
+    },
+    {
+      name: `FIND WORKER`,
+      desc: `If you are a Business looking for solutions to your Staffing
+            needs, be it a Single Hire for a difficult to fill position or
+            Staffing support of your Contingent Work Force....`,
+      slogan: `YOU CAME TO THE RIGHT PLACE.`
+    }
   ]
-}
+};
 
 export default assets;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import './home.css';
+import assets from '../../assets/constants';
 
 class Home extends React.Component {
   render() {
     return (
       <main>
-        <div>
-          <h1>Home</h1>
-        </div>
+        <section className="staffing">
+          <h1 className="title">
+            Recruitment &amp; <br /> Staffing solutions
+          </h1>
+          <div>
+            <Button variant="contained" color="primary">
+              Learn more
+            </Button>
+          </div>
+        </section>
+        <section className="find">
+          {assets.marketing.map((item, index) => {
+            return (
+              <div className="marketing">
+                <h2 className="marketing-name">{item.name}</h2>
+                <p>{item.desc}</p>
+                <h4>{item.slogan}</h4>
+                <Button variant="contained">
+                  Learn more
+                </Button>
+              </div>
+            );
+          })}
+        </section>
       </main>
     );
   }
