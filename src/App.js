762cd8add229d17d6f324b81e2fabe7f71f72c28
Puneet Kashyap <pkashyaps@gmail.com>
2019-04-25 11:27:32 -0400

    Scroll to top component added for route transition

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
 import Register from './components/register/Register';
-import './App.css';
 import Contact from './components/contact/Contact';
 import AboutUs from './components/aboutUs/AboutUs';
+import ScrollToTop from './components/ScrollToTop';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <Header />
-          <Route path="/" exact component={Home} />
-          <Route path="/home" exact component={Home} />
-          <Route path="/register" exact component={Register} />
-          <Route path="/about" exact component={AboutUs} />
-          <Route path="/contact" exact component={Contact} />
-          <Footer />
+          <ScrollToTop>
+            <Header />
+            <Route path="/" exact component={Home} />
+            <Route path="/home" exact component={Home} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/about" exact component={AboutUs} />
+            <Route path="/contact" exact component={Contact} />
+            <Footer />
+          </ScrollToTop>
         </Router>
       </div>
     );
