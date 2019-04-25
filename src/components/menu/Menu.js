32489fe6473fc67ddd4e9fdcdedab40d31a1c1ad
Puneet Kashyap <pkashyaps@gmail.com>
2019-04-25 09:57:14 -0400

    Update menu paths

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 import './menu.css';
 
 class Menu extends React.Component {
-  menuItem = ['Home', 'Register', 'Jobs', 'Contact'];
+  menuItem = [
+    { title: 'Home', path: '/' },
+    { title: 'Register', path: '/register' },
+    { title: 'About Us', path: '/about' },
+    { title: 'Contact', path: '/contact' },
+  ];
   render() {
     return (
       <nav className="menu">
         {this.menuItem.map((item, index) => {
           return (
             <div key={index} className="menu-item">
-               <Link className="menu-link" to={`/${item}`}>{item}</Link>
+              <Link className="menu-link" to={`${item.path}`}>
+                {item.title}
+              </Link>
             </div>
           );
         })}
