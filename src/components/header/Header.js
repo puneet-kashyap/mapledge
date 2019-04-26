3f68f582e4c9f36a1a60dddaadfafb88affa03ac
Puneet Kashyap <pkashyaps@gmail.com>
2019-04-26 09:38:11 -0400

    Header logo linked to home page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import './header.css';
+import { Link } from 'react-router-dom';
+
 import logo from '../../images/logo.png';
 import Menu from '../menu/Menu';
+import './header.css';
 
 class Header extends React.Component {
   render() {
@@ -10,7 +12,9 @@ class Header extends React.Component {
         <header>
           <div className="logo">
             <img className="logo-img" src={logo} alt="Logo" />
-            <h1 className="brand">Mapledge Staffing</h1>
+            <Link to="/">
+              <h1 className="brand">Mapledge Staffing</h1>
+            </Link>
           </div>
         </header>
         <Menu />
